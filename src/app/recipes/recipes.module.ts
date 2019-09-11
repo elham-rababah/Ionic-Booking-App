4bc856e6b816062dcda03ffe89ecf4f895752eaf
elham Rababah <elham@model.com.su>
2019-09-11 16:42:29 +0300

    fix small bug

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { IonicModule } from "@ionic/angular";
 
 import { RecipesPage } from "./recipes.page";
+import { RecipesItemComponent } from "./recipes-item/recipes-item.component";
 
 const routes: Routes = [
   {
@@ -19,8 +20,9 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+
     RouterModule.forChild(routes)
   ],
-  declarations: [RecipesPage]
+  declarations: [RecipesItemComponent, RecipesPage]
 })
 export class RecipesPageModule {}
