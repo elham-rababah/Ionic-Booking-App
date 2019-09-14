4592c2d635b20203c8ba930f5b942d305a223d55
elham Rababah <elham@model.com.su>
2019-09-14 14:08:58 +0300

    add Swipeable List Items

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class AuthService {
-  private userIsAuthenticate = false;
+  private userIsAuthenticate = true;
   constructor() {}
   login() {
     this.userIsAuthenticate = true;
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Place } from "../places.model";
 import { PlacesService } from "../places.service";
+import { IonItemSliding } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-offers",
@@ -9,9 +11,14 @@ import { PlacesService } from "../places.service";
 })
 export class OffersPage implements OnInit {
   loadedOffers: Place[];
-  constructor(private placesService: PlacesService) {}
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.loadedOffers = this.placesService.getAllPlaces();
+  }
+
+  onEdit(id, ref: IonItemSliding) {
+    ref.close();
+    this.router.navigateByUrl("/places/tabs/offers/edit/" + id);
   }
 }
